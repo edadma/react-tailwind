eb36e3b44a663db713aa9c5443e84abaa3914d64
Edward A Maxedon Sr <edadma@gmail.com>
2022-01-14 11:05:05 -0500

    add dark mode focus ring color

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -24,7 +24,7 @@ export const DEFAULT_THEME = {
       'bg-green-600 dark:bg-green-500',
       'hover:bg-green-600',
       'hover:bg-green-800 dark:hover:bg-green-700',
-      'focus:ring-green-300',
+      'focus:ring-green-300 dark:focus:ring-green-800',
       'border border-green-500',
       'hover:text-green-50'
     ),
@@ -34,7 +34,7 @@ export const DEFAULT_THEME = {
       'bg-blue-600 dark:bg-blue-500',
       'hover:bg-blue-600',
       'hover:bg-blue-800 dark:hover:bg-blue-700',
-      'focus:ring-blue-300',
+      'focus:ring-blue-300 dark:focus:ring-blue-800',
       'border border-blue-500',
       'hover:text-blue-50'
     ),
@@ -44,7 +44,7 @@ export const DEFAULT_THEME = {
       'bg-gray-600 dark:bg-gray-500',
       'hover:bg-gray-600',
       'hover:bg-gray-800 dark:hover:bg-gray-700',
-      'focus:ring-gray-300',
+      'focus:ring-gray-300 dark:focus:ring-gray-800',
       'border border-gray-500',
       'hover:text-gray-50'
     ),
@@ -54,7 +54,7 @@ export const DEFAULT_THEME = {
       'bg-indigo-600 dark:bg-indigo-500',
       'hover:bg-indigo-600',
       'hover:bg-indigo-800 dark:hover:bg-indigo-700',
-      'focus:ring-indigo-300',
+      'focus:ring-indigo-300 dark:focus:ring-indigo-800',
       'border border-indigo-500',
       'hover:text-indigo-50'
     ),
@@ -64,7 +64,7 @@ export const DEFAULT_THEME = {
       'bg-blue-600 dark:bg-blue-500',
       'hover:bg-blue-600',
       'hover:bg-blue-800 dark:hover:bg-blue-700',
-      'focus:ring-blue-300',
+      'focus:ring-blue-300 dark:focus:ring-blue-800',
       'border border-blue-500',
       'hover:text-blue-50'
     ),
@@ -74,7 +74,7 @@ export const DEFAULT_THEME = {
       'bg-green-600 dark:bg-green-500',
       'hover:bg-green-600',
       'hover:bg-green-800 dark:hover:bg-green-700',
-      'focus:ring-green-300',
+      'focus:ring-green-300 dark:focus:ring-green-800',
       'border border-green-500',
       'hover:text-green-50'
     ),
@@ -84,7 +84,7 @@ export const DEFAULT_THEME = {
       'bg-yellow-600 dark:bg-yellow-500',
       'hover:bg-yellow-600',
       'hover:bg-yellow-800 dark:hover:bg-yellow-700',
-      'focus:ring-yellow-300',
+      'focus:ring-yellow-300 dark:focus:ring-yellow-800',
       'border border-yellow-500',
       'hover:text-yellow-50'
     ),
@@ -94,7 +94,7 @@ export const DEFAULT_THEME = {
       'bg-red-600 dark:bg-red-500',
       'hover:bg-red-600',
       'hover:bg-red-800 dark:hover:bg-red-700',
-      'focus:ring-red-300',
+      'focus:ring-red-300 dark:focus:ring-red-800',
       'border border-red-500',
       'hover:text-red-50'
     ),
