4096d089d719e2091d94f72232821a876f24863e
Edward A Maxedon Sr <edadma@gmail.com>
2022-01-20 18:15:51 -0500

    start coding Input

--- src/Form.tsx
+++ src/Form.tsx
@@ -26,74 +26,60 @@ export const Form: React.FC<FormProps> = ({ children, onSubmit }) => {
   )
 }
 
-interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  outlined?: boolean
+interface InputProps
+  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   rounded?: boolean
   pill?: boolean
   icon?: boolean
   transition?: boolean
   role?: Color
-  size?: Size
+  inputSize?: Size
   weight?: Weight
+    label?:string
 }
 
-export const Button: React.FC<ButtonProps> = ({
+export const Input: React.FC<InputProps> = ({
   children,
   className,
   role,
-  outlined,
-  size,
-  weight,
+  inputSize,
+  weight,label,type,
   ...other
 }) => {
   const { theme } = useTheme()
 
   return (
-    <button
-      type="button"
-      className={
-        (outlined
-          ? `${colorClass(theme, 'button', role, 'outlinedBorder')} ${colorClass(
-              theme,
-              'button',
-              role,
-              'outlinedText'
-            )} ${colorClass(theme, 'button', role, 'outlinedHoverBg')} ${colorClass(
-              theme,
-              'button',
-              role,
-              'outlinedHoverText'
-            )}`
-          : `${colorClass(theme, 'button', role, 'filledBg')} ${colorClass(
-              theme,
-              'button',
-              role,
-              'filledText'
-            )} ${colorClass(theme, 'button', role, 'filledHoverBg')}`) +
-        ` ${sizeClass(theme, 'button', size, 'text')} ${colorClass(
-          theme,
-          'button',
-          role,
-          'focusRing'
-        )} ${optionProps(
-          theme,
-          other,
-          'button',
-          'rounded',
-          'pill',
-          'icon',
-          'transition'
-        )} ${weightClass(theme, 'button', weight)} ${theme.component.button.style} ${
-          className || ''
-        }`
-      }
+      <div className="mb-6">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{label}</label>
+          <input type={type} id="email"
+                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 placeholder="name@flowbite.com" required>
+          <input
+      className={`${colorClass(theme, 'input', role, 'filledBg')} ${colorClass(
+        theme,
+        'input',
+        role,
+        'filledText'
+      )} ${colorClass(theme, 'input', role, 'filledHoverBg')} ${sizeClass(
+        theme,
+        'input',
+        inputSize,
+        'text'
+      )} ${colorClass(theme, 'input', role, 'focusRing')} ${optionProps(
+        theme,
+        other,
+        'input',
+        'rounded',
+        'pill',
+        'icon',
+        'transition'
+      )} ${weightClass(theme, 'input', weight)} ${theme.component.input.style} ${
+        className || ''
+      }`}
       {...other}
     >
       {children}
-    </button>
+    </input>
+      </div>
   )
 }
