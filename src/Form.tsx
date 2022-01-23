b7f8c28712e45e8fb6109d63178fc464864e621a
Edward A Maxedon Sr <edadma@gmail.com>
2022-01-23 12:21:12 -0500

    cont coding Form

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,29 +1,17 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Color, Size, Weight } from './types'
 import { colorClass, optionProps, sizeClass, useTheme, weightClass } from './ThemeProvider'
+import { useFormik } from 'formik'
 
 interface FormProps {
   onSubmit: (data: any) => void
+  initialValues: any
 }
 
-export const Form: React.FC<FormProps> = ({ children, onSubmit }) => {
-  const form = useRef<HTMLFormElement>(null)
+export const Form: React.FC<FormProps> = ({ children, onSubmit, initialValues }) => {
+  const formik = useFormik({ onSubmit, initialValues })
 
-  return (
-    <form
-      ref={form}
-      onSubmit={(e) => {
-        e.preventDefault()
-
-        const data = new FormData(e.target as HTMLFormElement)
-
-        onSubmit(Object.fromEntries(data.entries()))
-        form.current?.reset()
-      }}
-    >
-      {children}
-    </form>
-  )
+  return <form onSubmit={formik.handleSubmit}>{children}</form>
 }
 
 interface InputProps
@@ -38,48 +26,55 @@ interface InputProps
   label?: string
 }
 
-// export const Input: React.FC<InputProps> = ({
-//   children,
-//   className,
-//   role,
-//   inputSize,
-//   weight,label,type,
-//   ...other
-// }) => {
-//   const { theme } = useTheme()
-//
-//   return (
-//       <div className="mb-6">
-//           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{label}</label>
-//           <input type={type} id="email"
-//                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//                  placeholder="name@flowbite.com" required>
-//           <input
-//       className={`${colorClass(theme, 'input', role, 'filledBg')} ${colorClass(
-//         theme,
-//         'input',
-//         role,
-//         'filledText'
-//       )} ${colorClass(theme, 'input', role, 'filledHoverBg')} ${sizeClass(
-//         theme,
-//         'input',
-//         inputSize,
-//         'text'
-//       )} ${colorClass(theme, 'input', role, 'focusRing')} ${optionProps(
-//         theme,
-//         other,
-//         'input',
-//         'rounded',
-//         'pill',
-//         'icon',
-//         'transition'
-//       )} ${weightClass(theme, 'input', weight)} ${theme.component.input.style} ${
-//         className || ''
-//       }`}
-//       {...other}
-//     >
-//       {children}
-//     </input>
-//       </div>
-//   )
-// }
+export const Input: React.FC<InputProps> = ({
+  children,
+  className,
+  role,
+  inputSize,
+  weight,
+  label,
+  type,
+  ...other
+}) => {
+  const { theme } = useTheme()
+
+  return (
+    <div className="mb-6">
+      {label && (
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`${colorClass(theme, 'input', role, 'filledBg')} ${colorClass(
+          theme,
+          'input',
+          role,
+          'filledText'
+        )} ${colorClass(theme, 'input', role, 'filledHoverBg')} ${sizeClass(
+          theme,
+          'input',
+          inputSize,
+          'text'
+        )} ${colorClass(theme, 'input', role, 'focusRing')} ${optionProps(
+          theme,
+          other,
+          'input',
+          'rounded',
+          'pill',
+          'icon',
+          'transition'
+        )} ${weightClass(theme, 'input', weight)} ${theme.component.input.style} ${
+          className || ''
+        }`}
+        {...other}
+      >
+        {children}
+      </input>
+    </div>
+  )
+}
